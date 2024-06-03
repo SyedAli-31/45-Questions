550055ecb81616416a6c873ea15c974243b4baa1
SyedAli-31 <164929736+SyedAli-31@users.noreply.github.com>
2024-06-03 23:49:01 +0500

    Add files via upload

---
+++ 11_name.js
@@ -0,0 +1,13 @@
+/* Question 11
+Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.*/
+let names = ["Ali", "mesum", "mohib"];
+// console.log(names)
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+export {};
+//with loop method
+// for(let i=0;i<names.length;i++){
+//  let friend=names[i]
+//  console.log(friend)
+// }
